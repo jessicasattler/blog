f634fe3a2fbbb3adc4d36d8bce42f2bddfa8bb7f
Jessica Sattler <jessicasattler28@gmail.com>
2017-11-06 22:09:46 -0600

    Added rest of pictures for across, time, distance

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,17 +8,20 @@
 	<div class="containerDiv">
 		<div class="divStyleA col-sm-6">
 			<h2>CONVERSATIONS</h2>
-			<img src="/images/conversations.jpg" placeholder="conversations">
+			<img src="/images/conversationsTwo.jpg" placeholder="conversations">
 		</div>
 		
 		<div class="divStyleA placeRight col-sm-offset-6 col-sm-6">
 			<h2>ACROSS</h2>
+			<img src="/images/acrossTwo.jpg" placeholder="across">
 		</div>
 		<div class="divStyleA col-sm-6">
 			<h2>TIME</h2>
+			<img src="/images/distanceTwo.jpg" placeholder="distance">
 		</div>
 		<div class="divStyleA col-sm-offset-6 col-sm-6">
 			<h2>AND DISTANCE</h2>
+			<img src="/images/timeTwo.jpg" placeholder="distance">
 		</div>
 		
 	</div>
