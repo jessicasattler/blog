160e8d6fb60460b8ea84680d7aed38d0a11a584b
Jessica Sattler <jessicasattler28@gmail.com>
2017-11-15 22:50:26 -0600

    Set up carousel and centered picture on carousel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,28 +5,77 @@
 @stop
 @section('content')
 	
-	<div class="containerDiv">
+{{-- 	<div class="containerDiv">
 
 		<div class="divStyleA col-sm-6 col-md-12">
 			<text class="right">CONVERSATIONS</text>
 			<img src="/images/conversationsTwo.jpg" placeholder="conversations">
 		</div>
 		<text id="across">ACROSS</text>
-		<div class="divStyleA placeRight col-sm-offset-6 col-sm-6">
+		<div class="divStyleA placeRight col-sm-offset-6 col-sm-6"> --}}
 			{{-- <text>ACROSS</text> --}}
-			<img src="/images/acrossTwo.jpg" placeholder="across">
+			{{-- <img src="/images/acrossTwo.jpg" placeholder="across">
 		</div>
 		<div class="divStyleA col-sm-6 col-md-12">
 			<text class="right">TIME</text>
 			<img src="/images/distanceTwo.jpg" placeholder="distance">
-		</div>
+		</div> --}}{{-- 
 		<text class="col-sm-12">AND DISTANCE</text>
-		<div class="divStyleA col-sm-offset-6 col-sm-6 col-md-12">
+		<div class="divStyleA col-sm-offset-6 col-sm-6 col-md-12"> --}}
 			{{-- <text>AND DISTANCE</text> --}}
-			<img src="/images/timeTwo.jpg" placeholder="distance">
-		</div>
+			{{-- <img src="/images/timeTwo.jpg" placeholder="distance">
+		</div> --}}
 		
-	</div>
+	{{-- </div> --}}
+
+	{{-- test carousel --}}
+	<div class="container">
+  <h2>Carousel Example</h2>  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        {{-- <img src="la.jpg" alt="Los Angeles" style="width:100%;"> --}}
+        <img src="/images/conversations.jpg" placeholder="conversations" class="center">
+      </div>
+
+      <div class="item">
+        {{-- <img src="chicago.jpg" alt="Chicago" class="center"> --}}
+        <img src="/images/across.jpg" placeholder="conversations" class="center">
+      </div>
+    
+      <div class="item">
+        {{-- <img src="ny.jpg" alt="New york" class="center"> --}}
+        <img src="/images/distance.jpg" placeholder="conversations" class="center">
+
+      </div>
+
+      <div class="item">
+        {{-- <img src="ny.jpg" alt="New york" class="center"> --}}
+        <img src="/images/time.jpg" placeholder="conversations" class="center">
+
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
 
 
 
--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -4,11 +4,18 @@
 	<title>@yield('title')</title>
 	 {{-- Google Font --}}
 	 {{-- <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"><link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"> --}}
-	 <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
+	 {{-- <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet"> --}}
 	 {{-- Minified Bootstrap --}}
-	 <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+	 {{-- <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"> --}}
 	 {{-- Main CSS  --}}
-	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	{{-- <link rel="stylesheet" type="text/css" href="/css/main.css"> --}}
+
+
+	  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
 
