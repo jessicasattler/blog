d8b949c65964fc6d16fcaf213057f9a93f738232
Jessica Sattler <jessicasattler28@gmail.com>
2018-02-03 12:11:37 -0600

    Added more sample entries to landing blog page

--- resources/views/newsfeed.blade.php
+++ resources/views/newsfeed.blade.php
@@ -7,8 +7,8 @@
 	
 
 <div class="form-group col-md-6 col-md-offset-2">
-	
-		<form>
+
+		
 		  
 		    <h2>Post Name</h2>
 		    <br>
@@ -31,9 +31,69 @@
 			  </div>
 
 			  <br>
-		  <button type="submit" class="btn btn-primary">Submit</button>
+		 <form name="star" method="POST" action"#"">
+		  <button type="submit" class="btn btn-primary">Star</button>
 		</form>
 </div>
+<div class="form-group col-md-6 col-md-offset-2">
+
+		
+		  
+		    <h2>Post Name</h2>
+		    <br>
+		    <h5>01/15/2018</h5>
+		    
+		  
+			  <div class="form-check">
+			    <label class="form-check-label">
+			      {{-- <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea> --}}
+
+			      
+			    </label>
+			    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+
+			  </div>
+
+			  <br>
+		 <form name="starTwo" method="POST" action"#"">
+		  <button type="submit" class="btn btn-primary">Star</button>
+		</form>
+</div>
+<div class="form-group col-md-6 col-md-offset-2">
+
+		
+		  
+		    <h2>Post Name</h2>
+		    <br>
+		    <h5>01/15/2018</h5>
+		    
+		  
+			  <div class="form-check">
+			    <label class="form-check-label">
+			      {{-- <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea> --}}
+
+			      
+			    </label>
+			    <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+
+			  </div>
+
+			  <br>
+		 <form name="starThree" method="POST" action"#"">
+		  <button type="submit" class="btn btn-primary">Star</button>
+		</form>
+</div>
+
 
 	
 @stop 
